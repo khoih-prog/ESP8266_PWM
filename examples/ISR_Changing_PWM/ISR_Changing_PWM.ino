8317624cdb3235584ece6f103a9b94207b78621e
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-11-06 19:31:36 -0400

    Add ISR_Changing_PWM example

---
+++ examples/ISR_Changing_PWM/ISR_Changing_PWM.ino
@@ -0,0 +1,175 @@
+/****************************************************************************************************************************
+  ISR_16_PWMs_Array_Simple.ino
+  For ESP8266 boards
+  Written by Khoi Hoang
+
+  Built by Khoi Hoang https://github.com/khoih-prog/ESP8266_PWM
+  Licensed under MIT license
+
+  The ESP8266 timers are badly designed, using only 23-bit counter along with maximum 256 prescaler. They're only better than UNO / Mega.
+  The ESP8266 has two hardware timers, but timer0 has been used for WiFi and it's not advisable to use. Only timer1 is available.
+  The timer1's 23-bit counter terribly can count only up to 8,388,607. So the timer1 maximum interval is very short.
+  Using 256 prescaler, maximum timer1 interval is only 26.843542 seconds !!!
+
+  Now with these new 16 ISR-based timers, the maximum interval is practically unlimited (limited only by unsigned long miliseconds)
+  The accuracy is nearly perfect compared to software timers. The most important feature is they're ISR-based timers
+  Therefore, their executions are not blocked by bad-behaving functions / tasks.
+  This important feature is absolutely necessary for mission-critical tasks.
+
+  Version: 1.0.0
+
+  Version Modified By   Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang      21/09/2021 Initial coding for ESP8266 boards with ESP8266 core v3.0.2+
+*****************************************************************************************************************************/
+
+#if !defined(ESP8266)
+#error This code is designed to run on ESP8266 and ESP8266-based boards! Please check your Tools->Board setting.
+#endif
+
+// These define's must be placed at the beginning before #include "ESP8266_PWM.h"
+// _PWM_LOGLEVEL_ from 0 to 4
+// Don't define _PWM_LOGLEVEL_ > 0. Only for special ISR debugging only. Can hang the system.
+#define _PWM_LOGLEVEL_                4
+
+#define USING_MICROS_RESOLUTION       true    //false 
+
+#include "ESP8266_PWM.h"
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN       2
+#endif
+
+#define HW_TIMER_INTERVAL_US      20L
+
+volatile uint32_t startMicros = 0;
+
+// Init ESP8266Timer
+ESP8266Timer ITimer;
+
+// Init ESP8266_ISR_PWM
+ESP8266_PWM ISR_PWM;
+
+void IRAM_ATTR TimerHandler()
+{
+  ISR_PWM.run();
+}
+
+//////////////////////////////////////////////////////
+
+//PIN_D0 can't be used for PWM/I2C
+#define PIN_D0            16        // Pin D0 mapped to pin GPIO16/USER/WAKE of ESP8266. This pin is also used for Onboard-Blue LED. PIN_D0 = 0 => LED ON
+#define PIN_D1            5         // Pin D1 mapped to pin GPIO5 of ESP8266
+#define PIN_D2            4         // Pin D2 mapped to pin GPIO4 of ESP8266
+#define PIN_D3            0         // Pin D3 mapped to pin GPIO0/FLASH of ESP8266
+#define PIN_D4            2         // Pin D4 mapped to pin GPIO2/TXD1 of ESP8266
+//#define PIN_LED           2         // Pin D4 mapped to pin GPIO2/TXD1 of ESP8266, NodeMCU and WeMoS, control on-board LED
+#define PIN_D5            14        // Pin D5 mapped to pin GPIO14/HSCLK of ESP8266
+#define PIN_D6            12        // Pin D6 mapped to pin GPIO12/HMISO of ESP8266
+#define PIN_D7            13        // Pin D7 mapped to pin GPIO13/RXD2/HMOSI of ESP8266
+#define PIN_D8            15        // Pin D8 mapped to pin GPIO15/TXD2/HCS of ESP8266
+#define PIN_D9            3         // Pin D9 /RX mapped to pin GPIO3/RXD0 of ESP8266
+#define PIN_D10           1         // Pin D10/TX mapped to pin GPIO1/TXD0 of ESP8266
+
+//Don't use pins GPIO6 to GPIO11 as already connected to flash, etc. Use them can crash the program
+//GPIO9(D11/SD2) and GPIO11 can be used only if flash in DIO mode ( not the default QIO mode)
+#define PIN_D11           9         // Pin D11/SD2 mapped to pin GPIO9/SDD2 of ESP8266
+#define PIN_D12           10        // Pin D12/SD3 mapped to pin GPIO10/SDD3 of ESP8266
+
+//////////////////////////////////////////////////////
+
+#define USING_PWM_FREQUENCY     true
+
+//////////////////////////////////////////////////////
+
+// You can assign pins here. Be carefull to select good pin to use or crash
+uint32_t PWM_Pin    = LED_BUILTIN;
+
+// You can assign any interval for any timer here, in microseconds
+uint32_t PWM_Period1 = 1000000L;
+// You can assign any interval for any timer here, in microseconds
+uint32_t PWM_Period2 = 500000L;
+
+// You can assign any interval for any timer here, in Hz
+uint32_t PWM_Freq1   = 1;
+// You can assign any interval for any timer here, in Hz
+uint32_t PWM_Freq2   = 2;
+
+// You can assign any duty_cycle for any PWM here, from 0-100
+uint32_t PWM_DutyCycle1  = 50;
+// You can assign any duty_cycle for any PWM here, from 0-100
+uint32_t PWM_DutyCycle2  = 90;
+
+// Channel number used to identify associated channel
+int channelNum;
+
+////////////////////////////////////////////////
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial);
+
+  delay(2000);
+
+  Serial.print(F("\nStarting ISR_Changing_PWM on ")); Serial.println(ARDUINO_BOARD);
+  Serial.println(ESP8266_PWM_VERSION);
+  Serial.print(F("CPU Frequency = ")); Serial.print(F_CPU / 1000000); Serial.println(F(" MHz"));
+
+  // Interval in microsecs
+  if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_US, TimerHandler))
+  {
+    startMicros = micros();
+    Serial.print(F("Starting ITimer OK, micros() = ")); Serial.println(startMicros);
+  }
+  else
+    Serial.println(F("Can't set ITimer. Select another freq. or timer"));
+
+  // Just to demonstrate, don't use too many ISR Timers if not absolutely necessary
+  // You can use up to 16 timer for each ISR_PWM
+  //void setPWM(uint32_t pin, uint32_t frequency, uint32_t dutycycle
+  // , timer_callback_p StartCallback = nullptr, timer_callback_p StopCallback = nullptr)
+}
+
+void loop()
+{ 
+  Serial.print(F("Using PWM Freq = ")); Serial.print(PWM_Freq1); Serial.print(F(", PWM DutyCycle = ")); Serial.println(PWM_DutyCycle1);
+  
+#if USING_PWM_FREQUENCY
+
+  // You can use this with PWM_Freq in Hz
+  ISR_PWM.setPWM(PWM_Pin, PWM_Freq1, PWM_DutyCycle1);
+
+#else
+  #if USING_MICROS_RESOLUTION
+  // Or using period in microsecs resolution
+  channelNum = ISR_PWM.setPWM_Period(PWM_Pin, PWM_Freq1, PWM_DutyCycle1);
+  #else
+  // Or using period in millisecs resolution
+  channelNum = ISR_PWM.setPWM_Period(PWM_Pin, PWM_Period1 / 1000, PWM_DutyCycle1);
+  #endif
+#endif
+
+  delay(10000);
+
+  ISR_PWM.deleteChannel((unsigned) channelNum);
+
+  Serial.print(F("Using PWM Freq = ")); Serial.print(PWM_Freq2); Serial.print(F(", PWM DutyCycle = ")); Serial.println(PWM_DutyCycle2);
+
+#if USING_PWM_FREQUENCY
+
+  // You can use this with PWM_Freq in Hz
+  ISR_PWM.setPWM(PWM_Pin, PWM_Freq2, PWM_DutyCycle2);
+
+#else
+  #if USING_MICROS_RESOLUTION
+  // Or using period in microsecs resolution
+  channelNum = ISR_PWM.setPWM_Period(PWM_Pin, PWM_Freq2, PWM_DutyCycle2);
+  #else
+  // Or using period in millisecs resolution
+  channelNum = ISR_PWM.setPWM_Period(PWM_Pin, PWM_Period2 / 1000, PWM_DutyCycle2);
+  #endif
+#endif
+
+  delay(10000);
+}
